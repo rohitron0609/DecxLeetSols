fd2a559a8ac2d4392c50c85fa5e7d2496b66ffe8
Rohit Yadav <rohitron0609@gmail.com>
2025-07-29 17:23:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,24 +2,21 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int l1 = s1.length();
         int l2 = s2.length();
-        if(l2 < l1) return false;
         int[] freqS1 = new int[26];
+        int[] freqWindow = new int[26];
+
         for(int i = 0; i < l1; i++){
             freqS1[s1.charAt(i)-'a']++;
+            freqWindow[s2.charAt(i)-'a']++;
         }
-        for(int i = 0; i <= l2-l1; i++){
-            int[] temp = createMap(s2,i,i+l1);
-            if(Arrays.equals(temp, freqS1))
+        if(Arrays.equals(freqS1, freqWindow)) return true;
+
+        for(int i = l1; i < l2; i++ ){
+            freqWindow[s2.charAt(i)-'a']++;
+            freqWindow[s2.charAt(i-l1)-'a']--;
+            if(Arrays.equals(freqS1, freqWindow))
                 return true;
         }
         return false;
-    }
-
-    int[] createMap(String s,int a, int b){
-        int[] freq = new int[26];
-        for(int i = a; i < b; i++){
-            freq[s.charAt(i)-'a']++;
-        }
-        return freq;
     }
 }
